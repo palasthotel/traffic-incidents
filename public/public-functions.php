fe7334a8aa436491614df9aeb4e344f2d3878f1e
EdwardBock <edward.bock@palasthotel.de>
2021-10-13 09:20:16 +0200

    public count function

--- public/public-functions.php
+++ public/public-functions.php
@@ -1,7 +1,14 @@
 <?php
 
+use Palasthotel\WordPress\TrafficIncidents\Model\IncidentQueryArgs;
 use Palasthotel\WordPress\TrafficIncidents\Plugin;
 
 function traffic_incidents_plugin(){
 	return Plugin::instance();
+}
+
+function traffic_incidents_get_incidents_count($post_id){
+	return count(traffic_incidents_plugin()->repo->queryIncidents(
+		IncidentQueryArgs::build($post_id)
+	));
 }
