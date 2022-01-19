c95bf1137e6c6f2e9d0b8b7db7a2dd2e13d26ede
EdwardBock <edward.bock@palasthotel.de>
2022-01-19 07:13:49 +0100

    check if schedule is already running

--- public/classes/Schedule.php
+++ public/classes/Schedule.php
@@ -8,10 +8,25 @@ use Palasthotel\WordPress\TrafficIncidents\Model\IncidentQueryArgs;
 
 class Schedule extends _Component {
 
+	private const TRANSIENT_KEY = "_traffic_incidents_is_fetching";
+
 	public function onCreate() {
 		parent::onCreate();
 		add_action( 'admin_init', [ $this, 'schedule' ] );
 		add_action( Plugin::SCHEDULE_FETCH_TRAFFIC_UPDATE, [ $this, 'run' ] );
+	}
+
+	private function setRunning( bool $is ) {
+		if ( $is ) {
+			set_transient( self::TRANSIENT_KEY, "1", MINUTE_IN_SECONDS * 10 );
+		} else {
+			delete_transient( self::TRANSIENT_KEY );
+		}
+
+	}
+
+	private function isRunning() {
+		return get_transient( self::TRANSIENT_KEY ) === "1";
 	}
 
 	public function schedule() {
@@ -21,26 +36,35 @@ class Schedule extends _Component {
 	}
 
 	public function run() {
+
+		if ( $this->isRunning() ) {
+			return;
+		}
+
+		$this->setRunning( true );
+
 		$posts = $this->plugin->repo->getPosts();
 
 		foreach ( $posts as $post ) {
-			$this->plugin->log->add("Fetch for post $post->ID");
+			$this->plugin->log->add( "Fetch for post $post->ID" );
 			$this->plugin->repo->fetchIncidents( $post->ID );
 
-			$incidents = $this->plugin->repo->queryIncidents(IncidentQueryArgs::build($post->ID));
-			$counter = 0;
-			foreach ($incidents as $incident){
+			$incidents = $this->plugin->repo->queryIncidents( IncidentQueryArgs::build( $post->ID ) );
+			$counter   = 0;
+			foreach ( $incidents as $incident ) {
 				$location = $incident->getStartLocation();
-				if(null === $location || !$location->isEmpty()){
+				if ( null === $location || ! $location->isEmpty() ) {
 					continue;
 				}
-				$counter++;
-				$this->plugin->repo->fetchLocation($location);
+				$counter ++;
+				$this->plugin->repo->fetchLocation( $location );
 
 			}
-			$count = count($incidents);
-			$this->plugin->log->add("Fetched locations for $counter of $count incidents");
+			$count = count( $incidents );
+			$this->plugin->log->add( "Fetched locations for $counter of $count incidents" );
 		}
+
+		$this->setRunning( false );
 
 	}
 
